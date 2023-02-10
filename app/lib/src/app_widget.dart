1fc8eac56f892b91cdc5a4f03fb745074dc30c71
Daniel Ernany <ddernany@gmail.com>
2023-02-10 17:23:12 -0300

    :sparkles: add tela de HomePage

---
+++ app/lib/src/app_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:listinha/src/home/home_page.dart';
+
+import 'shared/themes/themes.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Listinha',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      home: const HomePage(),
+    );
+  }
+}
